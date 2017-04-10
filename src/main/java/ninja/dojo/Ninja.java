e7d3a8ca1a2671de71333ecca2923472126773a2
Nicholas <atreayou@gmail.com>
2017-04-09 22:18:38 -0500

    other intial

---
+++ src/main/java/ninja/dojo/Ninja.java
@@ -0,0 +1,17 @@
+package ninja.dojo;
+
+ /**************************************************
+ ** Created by Nicholas on 4/9/2017.              **
+ **************************************************/
+public class Ninja
+{
+
+	private String name;
+
+	public Ninja(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName() { return name; }
+}
